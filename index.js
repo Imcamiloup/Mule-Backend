10f7002ca3cc026cb83d2f35e986f0ae92fe0a7a
carman12 <44560188+carman12@users.noreply.github.com>
2024-05-07 18:37:02 -0500

    Ajuste bd_port index

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import server from "./src/server.js";
 import sequelize from "./src/database/db.js";
-const { PORT } = process.env;
+const { DB_PORT } = process.env;
 
 sequelize
-  .sync({ alter: true })
+  .sync({ force: true })
   .then(() => {
-    server.listen(PORT, () => {
+    server.listen(DB_PORT, () => {
       console.log("Database connection succesful!");
-      console.log(`Server listening on port: http://localhost:${PORT}`);
+      console.log(`Server listening on port: http://localhost:${DB_PORT}`);
     });
   })
   .catch((error) => {
-    console.log("There was a mistake", error.message);
+    console.error("There was an error:", error);
+    console.log("There was a mistake"+`${DB_PORT}`, error.message);
   });
 
 export default sequelize;
