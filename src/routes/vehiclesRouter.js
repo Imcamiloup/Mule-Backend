0377f9a34c53e79ad37d8b1857de45626f1ef896
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-06-05 15:40:02 -0500

    Agregando POST a vehicles

--- src/routes/vehiclesRouter.js
+++ src/routes/vehiclesRouter.js
@@ -3,12 +3,14 @@ import {
   getVehiclesHandler,
   getVehicleByIdHandler,
   updateVehicleHandler,
+  createVehicleHandler,
 } from "../handlers/vehiclesHandler.js";
 
 const vehiclesRouter = Router();
 
 vehiclesRouter.get("/", getVehiclesHandler);
 vehiclesRouter.get("/:id", getVehicleByIdHandler);
+vehiclesRouter.post("/", createVehicleHandler);
 vehiclesRouter.patch("/:id", updateVehicleHandler);
 
 export default vehiclesRouter;
