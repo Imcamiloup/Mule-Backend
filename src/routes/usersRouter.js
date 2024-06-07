a3ca0a7121f6dcc9d0fe55cc8f04ecfad98aa430
Luis Gómez <luiscgr97@gmail.com>
2024-06-06 21:31:25 -0500

    resetPassword and confirmEmail set carlos

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -9,12 +9,11 @@ import {
   loginHandler,
   updateProfileHandler,
   genereteAuth0User,
-  getUserByNameHandler
+  getUserByNameHandler,
 } from "../handlers/usersHandler.js";
 import { confirmEmail } from "../email/sendgridController.js";
 import { authenticateToken } from "../utils/helperToken/authenticateToken.js";
-
-
+import { resetPassword , requestPasswordReset } from "../email/resetPassword.js";
 
 const usersRouter = Router();
 
@@ -26,8 +25,8 @@ usersRouter.get ("/name",getUserByNameHandler);
 usersRouter.post("/register", registerHandler);
 usersRouter.post("/register-auth0", genereteAuth0User);
 usersRouter.post("/login", loginHandler);
-usersRouter.post("/request-password-reset");
-usersRouter.post("/reset-password");
+usersRouter.patch("/reset-password", resetPassword);
+usersRouter.post("/request-password-reset", requestPasswordReset);
 usersRouter.patch("/:id", authenticateToken, updateUserHandler);
 usersRouter.patch("/profile/:id", updateProfileHandler)
 usersRouter.delete("/:id", authenticateToken, deleteUserHandler);
